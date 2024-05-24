f0a88fd1b9d2d0001176a323ea72815b03770024
rachitjaiswal <rachitjaiswal999@gmail.com>
2024-05-24 19:55:39 +0530

    i

--- vite-project/src/components/utils/ApiFunctions.js
+++ vite-project/src/components/utils/ApiFunctions.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:9192"
+	baseURL: "https://hotelbackend-dst7.onrender.com"
 })
 
 export const getHeader = () => {
